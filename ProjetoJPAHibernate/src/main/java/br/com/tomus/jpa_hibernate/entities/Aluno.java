b2d5400f8307d10cdd7daa264ab55ae30832ecdb
AlansidneyJunior <alansidney.silva21@gmail.com>
2024-12-18 09:40:16 -0300

    Criando Entidade Aluno.

---
+++ ProjetoJPAHibernate/src/main/java/br/com/tomus/jpa_hibernate/entities/Aluno.java
@@ -0,0 +1,52 @@
+package br.com.tomus.jpa_hibernate.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Alunos")
+public class Aluno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(unique = true, nullable = false)
+	private String cpf;
+	
+	private int idade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+}
